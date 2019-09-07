ff54fa1bcbd3b688e6444ad9e2df50bfe1ed9b53
Clay <cwells@cwells.org>
2019-09-06 22:34:12 -0400

    new: initial commit.

---
+++ ctfjawn-ba-01.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "libjawn.h"
+
+/*
+ * ctfjawn-ba19-01.c
+ *
+ * This is one of five binary challenges which are intended to teach those new
+ * to the world of binary analysis.
+ *
+ * Level: Easy
+ * Compile.. produce debugging information in the operating system's native
+ * format (stabs, COFF, XCOFF, or DWARF). GDB can work with this debugging
+ * information.
+ * gcc -g ctfjawn-ba-01.c
+ */
+
+/********* FUNCTIONS *********/
+void cm9sbHdpdGhyaWNr() {
+    char rwr[] = "just cause";
+    // TODO
+    printf("\n");
+}
+
+void c2VjcmV0 (){
+    char c2c[] = "10.100.67.7";
+    // TODO
+    printf("C2C: %s\n", C2C);
+}
+
+/********* MAIN *********/
+int main() {
+    int i = 0;
+
+    /*
+     * using gdb, run this program and set i=1 so that the function c2VjcmV0()
+     * executes
+     */
+    if (i == (6-5)) {
+        c2VjcmV0();
+    }
+
+    return 0;
+}
---
+++ libjawn.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+ * libjawn.h
+ *
+ * 
+ */
+
+#ifndef LIBJAWN_H
+#define LIBJAWN_H
+#endif
+
+#ifndef VERSION_H
+#define VERSION_H "0.1"
+#endif
+
+
+const char * C2C = "10.10.66.6";
+
