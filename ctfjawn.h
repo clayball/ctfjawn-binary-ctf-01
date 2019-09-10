c92322c315dd0e4e7e067947a54dedf513f7f851
Clay <cwells@cwells.org>
2019-09-10 11:04:12 -0400

    new: adds back ctfjawn.h.

---
+++ ctfjawn.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+ * libjawn.h
+ *
+ * 
+ */
+
+#ifndef LIBJAWN_H
+#define LIBJAWN_H
+
+
+/* TODO: encrypt these values, add decrypt function*/
+const char * SECRET = "dCTTSDrCU]E";
+const char * FLAG0x01 = "aHR0cDovL3d3dy5waHJhY2sub3JnL2lzc3Vlcy83LzMuaHRtbCNhcnRpY2xl";
+
+void decrypt_jawn();
+
+#endif
